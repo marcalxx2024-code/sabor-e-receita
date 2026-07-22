ab4ac72028a2177145a4ab565871b4feff7697b8
marcalxx2024-code <marcalxx2024@gmail.com>
2026-07-22 03:59:02 -0300

    test: remove StrictMode temporariamente para debug

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,12 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
 import App from './App';
 import { FavoritesProvider } from './contexts/FavoritesContext';
 import './index.css';
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <FavoritesProvider>
-        <App />
-      </FavoritesProvider>
-    </BrowserRouter>
-  </StrictMode>,
+  <BrowserRouter>
+    <FavoritesProvider>
+      <App />
+    </FavoritesProvider>
+  </BrowserRouter>,
 );
